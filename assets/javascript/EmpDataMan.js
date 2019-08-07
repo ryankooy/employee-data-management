33e8f813a3bcde0ee8fe2e6c6907ac7dd0426ff4
Ryan Kooy <rwkooy@gmail.com>
2019-08-06 21:06:20 -0400

    deleted extraneous comments

--- assets/javascript/EmpDataMan.js
+++ assets/javascript/EmpDataMan.js
@@ -58,34 +58,3 @@ $(document).ready(function() {
     });
         
 });
-
-
-
-
-
-
-
-
-
-
-
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-// <script src="https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js"></script>
-
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-//      https://firebase.google.com/docs/web/setup#config-web-app -->
-
-// <script>
-//   // Your web app's Firebase configuration
-//   var firebaseConfig = {
-//     apiKey: "AIzaSyCTIAppQqJZCBIsN3ok1UXGejv11Nfgzy8",
-//     authDomain: "projectification-21972.firebaseapp.com",
-//     databaseURL: "https://projectification-21972.firebaseio.com",
-//     projectId: "projectification-21972",
-//     storageBucket: "",
-//     messagingSenderId: "649772529131",
-//     appId: "1:649772529131:web:fbd2d6151a638a37"
-//   };
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-// </script>
